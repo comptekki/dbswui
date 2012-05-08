2ef6e3b7e9c3492bbe1bdee41bebc11fbec08281
comptekki <comptekki@gmail.com>
2012-05-08 14:01:26 -0600

    edit record now working

--- src/default_handler.erl
+++ src/default_handler.erl
@@ -6,7 +6,7 @@ init({_Any, http}, Req, []) ->
        {ok, Req, undefined}.
 
 handle(Req, State) ->
-	{ok,Req1} = cowboy_http_req:set_resp_header('Location', <<"db1">>, Req),
+	{ok,Req1} = cowboy_http_req:set_resp_header('Location', <<"/db1">>, Req),
 	{ok, Req2} = cowboy_http_req:reply(302, [], <<>>, Req1),
 	{ok, Req2, State}.
 
