10e1a013d7a13f6d53876a74e677f1389503896c
comptekki <comptekki@gmail.com>
2012-05-16 13:07:12 -0600

    add dynamic ports

--- src/dbswui.erl
+++ src/dbswui.erl
@@ -9,32 +9,32 @@ start() ->
 	application:start(cowboy),
 	application:start(dbswui).
 
-start(_Type, _Args) ->
+start(_Type, Args) ->
+	[HTTP_Port,HTTPS_Port]=
+		case Args of
+			[] -> [9080,9443];
+			Any -> Any
+		end,
 	Dispatch =
 		[
 		 {'_',
 		  [
 		   {[<<"db1">>], db1_handler, []},
-		   {[<<"db1">>,<<"edit">>], db1_edit_handler, []},
-		   {[<<"db1">>,<<"edit">>,<<"item">>], db1_edit_item_handler, []},
-		   {[<<"db1">>,<<"logout">>], logout_handler, []},
-		   {[<<"static">>,'...'],cowboy_http_static,[{directory, "static/"}]},
-		   {'_', logout_handler, []}
+		   {[<<"db1">>, <<"edit">>], db1_edit_handler, []},
+		   {[<<"db1">>, <<"logout">>], logout_handler, []},
+		   {[<<"static">>, '...'], cowboy_http_static, [{directory, "static/"}]},
+		   {'_', default_handler, []}
 		  ]
 		 }
 		],
 	cowboy:start_listener(
 	  my_http_listener, 100,
-	  cowboy_tcp_transport, [{port, 8080}],
+	  cowboy_tcp_transport, [{port, HTTP_Port}],
 	  cowboy_http_protocol, [{dispatch, Dispatch}]
 	 ),
 	cowboy:start_listener(
 	  my_https_listener, 100,
-	  cowboy_ssl_transport,
-	  [
-	   {port, 8443}, {certfile, "priv/ssl/cert.pem"},
-	   {keyfile, "priv/ssl/key.pem"}, {password, "cowboy"}
-	  ],
+	  cowboy_ssl_transport, [{port, HTTPS_Port}, {certfile, "priv/ssl/cert.pem"}, {keyfile, "priv/ssl/key.pem"}, {password, ""}],
 	  cowboy_http_protocol, [{dispatch, Dispatch}]
 	 ),
 	dbswui_sup:start_link().
