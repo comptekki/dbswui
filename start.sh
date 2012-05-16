dbaa0f979d7dd4a69102caa2dbddec0f927e8799
comptekki <comptekki@gmail.com>
2012-05-16 13:07:28 -0600

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 erl -sname dbswui -pa ebin -pa deps/*/ebin -s dbswui \
-	-eval "io:format(\"~n~nThe following examples are available:~n\")." \
-	-eval "io:format(\"* db1 : http://localhost:8080/db1~n\")."
+	-eval "io:format(\"~n~nThe following sites are available:~n\")." \
+	-eval "io:format(\"* db1 : http://localhost:9080/db1~n\")." \
+	-eval "io:format(\"* db1 : http://localhost:9443/db1~n\")."
