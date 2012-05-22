e3d157d2f6a77008e0d1b2d18567d0231d994f2d
comptekki <comptekki@gmail.com>
2012-05-22 15:57:25 -0600

    undo

--- start.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-erl -sname dbswui -pa ebin -pa deps/*/ebin -s dbswui \
-	-eval "io:format(\"~n~nThe following sites are available:~n\")." \
-	-eval "io:format(\"* db1 : http://localhost:9080/db1~n\")." \
-	-eval "io:format(\"* db1 : http://localhost:9443/db1~n\")."
