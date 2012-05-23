93d7077fdcd3381dba198143fcd9f9bef91830e4
comptekki <comptekki@gmail.com>
2012-05-23 11:16:06 -0600

    hilite page number

--- src/db_handler.erl
+++ src/db_handler.erl
@@ -2,7 +2,7 @@
 -behaviour(cowboy_http_handler).
 -export([init/3, handle/2, terminate/2]).
 
--import(dbswui_lib, [return_top_page/2, select_fields/1, select_pattern/1, select_pattern/3, table/6]).
+-import(dbswui_lib, [return_top_page/2, select_fields/1, select_pattern/1, select_pattern/3, table/7]).
 
 -include("deps/cowboy/include/http.hrl").
 -include("db.hrl").
@@ -21,13 +21,15 @@ handle(Req0, State) ->
 	{S, _Req} = cowboy_http_req:qs_val(<<"s">>,Req),
 	{Table, _Req} = cowboy_http_req:qs_val(<<"tablename">>,Req),
 	{ok, Req2} =
-		cowboy_http_req:reply(200, [],
+		cowboy_http_req:
+		reply(200, [],
 			  case Table of
 				  ?DB ->
 					  {Rpp, _Req} = cowboy_http_req:qs_val(<<"rpp">>,Req),
 					  {Offset, _Req} = cowboy_http_req:qs_val(<<"offset">>,Req),
+					  {N, _Req} = cowboy_http_req:qs_val(<<"n">>, Req),
 					  {FieldsAll, _Req} = cowboy_http_req:qs_vals(Req),
-					  [_,_,_,_|RawFields] = FieldsAll,
+					  [_,_,_,_,_|RawFields] = FieldsAll,
 					  Fields=select_fields(RawFields),
 					  Sp =
 						  case Fields of
@@ -35,7 +37,7 @@ handle(Req0, State) ->
 							  _  -> (catch select_pattern(Table, Fields, S))
 						  end,
 					  SpOffset = <<Sp/binary, " offset ", Offset/binary, " limit ", Rpp/binary>>,
-					  <<(table(Sp, SpOffset, Rpp, ServerPath, Fields, S))/binary>>;
+					  <<(table(Sp, SpOffset, Rpp, ServerPath, Fields, S, N))/binary>>;
 				  _ ->
 					  case S of
 						  <<"1">> ->
