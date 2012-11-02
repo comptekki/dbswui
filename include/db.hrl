74e3eef85d6bd6b2fd3e72664d9369038b63e5b8
comptekki <comptekki@gmail.com>
2012-11-02 13:17:04 -0600

    sample db.hrl

---
+++ include/db.hrl
@@ -0,0 +1,25 @@
+-define(DBHOST, "localhost").
+-define(HOST, "localhost").
+-define(DB, <<"dbooks">>).
+-define(USERNAME, "pguser").
+-define(PASSWORD, "").
+-define(PORT, 5432).
+-define(JQUERY, "/static/jquery-1.7.2.min.js").
+-define(DBTITLE, <<"Duplicate Books">>).
+-define(MAX_LENB, <<"300">>).
+-define(MAX_LEN, 300).
+-define(TABLE, 	[<<"title">>, <<"author_editor">>, <<"date_of_publication">>, <<"publisher">>, <<"key_words">>, <<"notes">>, <<"valuation">>, <<"purchase_price">>]).
+-define(ORDER_BY, " order by title").
+-define(TITLES,
+		case Title of
+			<<"title">> -> <<"Title">>;
+			<<"author_editor">> -> <<"Author Editor">>;
+			<<"date_of_publication">> -> <<"Date of Publication">>;
+			<<"publisher">> -> <<"Publisher">>;
+			<<"key_words">> -> <<"Key Words">>;
+			<<"notes">> -> <<"Notes">>;
+			<<"valuation">> -> <<"Valuation">>;
+			<<"purchase_price">> -> <<"Purchase Price">>
+		end).
+-define(CONF,"/usr/local/src/dbswui/src/db.conf").
+-define(MAXAGE, 500).
