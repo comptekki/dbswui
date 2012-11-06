811e8d6a0fb28808d27a245540d784a9638c84fb
comptekki <comptekki@gmail.com>
2012-11-06 09:30:57 -0700

    fix http to https on ssl link

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 erl -sname dbswui -pa ebin -pa deps/*/ebin -s dbswui \
 	-eval "io:format(\"~n~nThe following sites are available:~n\")." \
 	-eval "io:format(\"* db : http://localhost:7080/db~n\")." \
-	-eval "io:format(\"* db : http://localhost:7443/db~n\")."
+	-eval "io:format(\"* db : https://localhost:7443/db~n\")."
 	-eval "io:format(\"~n~nOr to Edit/Delete/Update Records:~n\")." \
 	-eval "io:format(\"* db : http://localhost:7443/db/edit~n\")."
