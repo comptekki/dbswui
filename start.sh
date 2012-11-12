7d864f948884768592d0156e7ab8b5218ae7948c
comptekki <comptekki@gmail.com>
2012-11-12 15:09:51 -0700

    change startup script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 erl -sname dbswui -pa ebin -pa deps/*/ebin -s dbswui \
-	-eval "io:format(\"~n~nThe following sites are available:~n\")." \
-	-eval "io:format(\"* db : http://localhost:7080/db~n\")." \
-	-eval "io:format(\"* db : https://localhost:7443/db~n\")." \
-	-eval "io:format(\"~n~nOr to Edit/Delete/Update Records:~n\")." \
-	-eval "io:format(\"* db : http://localhost:7443/db/edit~n\")."
+	-eval "io:format(\"~n~nThe following sites are available:~n\
+	* db : http://localhost:7080/db~n\
+	* db : https://localhost:7443/db~n\
+	~n~nOr to Edit/Delete/Update Records:~n\
+	* db : http://localhost:7443/db/edit~n~n\")."
